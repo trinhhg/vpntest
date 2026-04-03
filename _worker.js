4d2bd57b3b2ed1d88cd47cacb59fd6380c816d9e
Trịnh Hg <onepiecechapter@gmail.com>
2026-04-03 21:11:57 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,19 +2,19 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    // Nếu là Link Sub hoặc API -> Bẻ lái ngầm sang con Worker (Nơi chứa Database)
+    // Nếu là Link Sub hoặc API -> Bẻ lái ngầm sang con Worker TEST (Nơi chứa Database)
     if (url.pathname.startsWith("/v1") || url.pathname.startsWith("/v2") || url.pathname.startsWith("/api")) {
       const workerUrl = new URL(request.url);
       
-      // Đổi đích đến thành con Worker của ông
-      workerUrl.hostname = "vpn-api.doicucden.workers.dev";
+      // QUAN TRỌNG: Đổi dòng này thành domain của con Worker TEST bạn vừa tạo
+      workerUrl.hostname = "https://vpn-worker-test.doicucden.workers.dev/"; 
       
       // Chuyển tiếp toàn bộ gói tin sang Worker xử lý
       const newRequest = new Request(workerUrl.toString(), request);
       return fetch(newRequest);
     }
 
-    // Nếu khách truy cập bình thường -> Phục vụ file giao diện tĩnh (index.html, admin.html)
+    // Nếu truy cập bình thường -> Phục vụ file giao diện tĩnh (index.html, admin.html)
     return env.ASSETS.fetch(request);
   }
 };
